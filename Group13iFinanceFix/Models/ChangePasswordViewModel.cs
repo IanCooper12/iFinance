5e4d1be011b6106412e1e067a0bbd846e98b84fd
GreysonR <44911101+GreysonR@users.noreply.github.com>
2025-04-25 22:50:12 -0500

    Unify styles

--- Group13iFinanceFix/Models/ChangePasswordViewModel.cs
+++ Group13iFinanceFix/Models/ChangePasswordViewModel.cs
@@ -4,12 +4,15 @@ namespace Group13iFinanceFix.Models
 {
     public class ChangePasswordViewModel
     {
+        [Display(Name = "Current password")]
         [Required]
         public string CurrentPassword { get; set; }
 
+        [Display(Name = "New password")]
         [Required]
         public string NewPassword { get; set; }
 
+        [Display(Name = "Confirm new password")]
         [Required]
         [Compare("NewPassword", ErrorMessage = "Passwords do not match.")]
         public string ConfirmPassword { get; set; }
